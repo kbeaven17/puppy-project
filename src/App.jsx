02140b9ff7db9c8629a8ce472e30d2b09614568b
Kyle Beaven <kbeaven17@gmail.com>
2024-02-19 13:03:35 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
+import { puppyList } from "./data.js"
+
 
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log('puppy list', puppyList)
+  
   return (
     
-      <div>
-         </div>
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return <p key = {puppy.id}>{puppy.name}</p>
+          })
+        }
+        </div>
          )
       
-  
 }
+
+export default App;
+
 
 
